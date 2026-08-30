3085e7d60d2c36f99af6744b4346d93d5164e631
Chikorita Lover <cjgardels@gmail.com>
2026-08-30 15:09:32 -0500

    Add close button to tabs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ namespace CrystalLens
 {
     public partial class MainWindow : Window
     {
+        private MainViewModel ViewModel => (MainViewModel)DataContext;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,14 +29,14 @@ namespace CrystalLens
                 Queue<ASMCommand> commands = file.GetCommands(file.GetLabels().First());
                 EncounterTableMap encounterTables = EncounterTableMap.ReadASM(commands);
 
-                ((MainViewModel)DataContext).OpenFiles.Add(new ASMFileViewModel(file, encounterTables));
+                ViewModel.OpenFiles.Add(new ASMFileViewModel(file, encounterTables));
                 tabs.SelectedIndex = tabs.Items.Count - 1;
             }
         }
 
         private void Close_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            ((MainViewModel)DataContext).OpenFiles.RemoveAt(tabs.SelectedIndex);
+            ViewModel.OpenFiles.RemoveAt(tabs.SelectedIndex);
         }
 
         private void Close_CanExecute(object sender, CanExecuteRoutedEventArgs e)
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections.ObjectModel;
+﻿using CrystalLens.Commands;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace CrystalLens.ViewModels
 {
     public class MainViewModel
     {
         public ObservableCollection<ASMFileViewModel> OpenFiles { get; } = [];
+        public ICommand CloseTabCommand { get; }
+
+        public MainViewModel()
+        {
+            CloseTabCommand = new RelayCommand(CloseTab_Execute);
+        }
+
+        private void CloseTab_Execute(object parameter)
+        {
+            OpenFiles.Remove((ASMFileViewModel)parameter);
+        }
     }
 }
